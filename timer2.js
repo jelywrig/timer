14153e26e93b1e6f09fd06e9d4e20cac21148266
Jessie Wright <jessie.wright@gmail.com>
2020-04-07 23:27:43 +0000

    removed extraneous/inaccurate comments

--- timer2.js
+++ timer2.js
@@ -15,11 +15,8 @@ const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
 
-////////////
-// Event Handling for User Input
-////////////
 
-// on any input from stdin (standard input), output a "." to stdout
+
 stdin.on('data', (key) => {
   if(key === 'b') {
     console.log('beep!');
